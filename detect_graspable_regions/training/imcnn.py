2246a79dba4247f6d4b14adc4e701244fbbbb1b0
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-07-25 13:44:06 +0200

    Updated documentation

--- detect_graspable_regions/training/imcnn.py
+++ detect_graspable_regions/training/imcnn.py
@@ -4,6 +4,7 @@ from torch import nn
 
 
 class SegImcnn(nn.Module):
+    """A wrapper class for Imcnn models that learn to segment meshes."""
     def __init__(self, adapt_data, layer_conf=None):
         super().__init__()
 
@@ -21,10 +22,43 @@ class SegImcnn(nn.Module):
         )
 
     def forward(self, x):
+        """Forwards inputs through the model.
+
+        Parameters
+        ----------
+        x: (torch.Tensor, torch.Tensor)
+            The input to the model. First element is the mesh signal, second are the barycentric coordinates.
+        """
         return self.model.forward(x)
 
     def train_loop(self, dataset, loss_fn, optimizer, verbose=True, epoch=None):
+        """Training loop of the model.
+
+        Parameters
+        ----------
+        dataset: torch.IterableDataset
+            The dataset on which the IMCNN shall be trained.
+        loss_fn: Callable
+            The loss function to use during training.
+        optimizer: torch.optim.Optimizer
+            The optimizer for training.
+        verbose: bool
+            Whether to print intermediate training information during training.
+        epoch: int
+            The current epoch. Only used for printing training information.
+        """
         return self.model.train_loop(dataset, loss_fn, optimizer, verbose=verbose, epoch=epoch)
 
     def validation_loop(self, dataset, loss_fn, verbose=True):
+        """Validation loop of the model.
+
+        Parameters
+        ----------
+        dataset: torch.IterableDataset
+            The dataset on which the IMCNN shall be validated.
+        loss_fn: Callable
+            The loss function to use during training.
+        verbose: bool
+            Whether to print intermediate training information during training.
+        """
         return self.model.validation_loop(dataset, loss_fn, verbose=verbose)
