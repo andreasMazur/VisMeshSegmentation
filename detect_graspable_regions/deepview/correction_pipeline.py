9bbba06c4ef1bcc25271fb54f7178a7de659f46b
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-07-25 11:38:00 +0200

    Updated documentation

--- detect_graspable_regions/deepview/correction_pipeline.py
+++ detect_graspable_regions/deepview/correction_pipeline.py
@@ -27,7 +27,59 @@ def deep_view_iter(model,
                    correction_file_name,
                    embed_fn,
                    pred_wrapper):
-    """Correct the segmentation labels for one mesh."""
+    """Correct the segmentation labels for one mesh.
+
+    Parameters
+    ----------
+    model: SegImcnn
+        A segmentation IMCNN.
+    signal: torch.Tensor
+        The signal defined on the shape.
+    bc: torch.Tensor
+        The barycentric coordinates for the shape.
+    coordinates: torch.Tensor
+        The 3D-coordinates of the mesh vertices.
+    labels: torch.Tensor
+        The original ground truth labels for the mesh vertices.
+    idx: int
+        The mesh index.
+    amount_classes: int
+        The total amount of available classes
+    classes: np.ndarray
+        All class labels in an array.
+    embedding_shape: tuple
+        The shape of the embedding vectors that the model uses to predict vertex probabilities. E.g., (96,) for
+        96-dimensional embeddings.
+    embed_fn: Callable
+        The function that takes the IMCNN and returns the embeddings that the model uses to predict vertex class
+        probabilities.
+    class_dict: dict
+        A dictionary that maps class labels (integers) to their class names (strings).
+    batch_size: int
+        The batch-size for DeepView.
+    max_samples: int
+        TODO
+    resolution: int
+        TODO
+    interpolations: int
+        TODO
+    lam: float
+        TODO
+    cmap: str
+        The colormap to use in DeepView.
+    metric: str
+        TODO
+    disc_dist: bool
+        Whether to use the discriminative distance in DeepView.
+    interactive: bool
+        TODO
+    title: str
+        The plot title
+    correction_file_name: str
+        The filename of the CSV-file where to store correction data.
+    pred_wrapper: Callable
+        A function that returns the predicted probabilities for a mesh vertex of a given IMCNN.
+    """
     # Get model embeddings
     embeddings = embed_fn(model, [signal, bc])
 
@@ -88,7 +140,49 @@ def correction_pipeline(model,
                         interactive=False,
                         title=None,
                         correction_file_name=None):
-    """Use DeepView, IMCNNs and 3D Visualizations to interactively correct segmentation labels for mesh partnet_grasp."""
+    """Use DeepView, IMCNNs and 3D Visualizations to interactively correct segmentation labels for mesh partnet_grasp.
+
+    Parameters
+    ----------
+    model: SegImcnn
+        A segmentation IMCNN.
+    dataset: generator
+        Generator for the (uncorrected) Partnet-grasp dataset.
+    embedding_shape: tuple
+        The shape of the embedding vectors that the model uses to predict vertex probabilities. E.g., (96,) for
+        96-dimensional embeddings.
+    embed_fn: Callable
+        The function that takes the IMCNN and returns the embeddings that the model uses to predict vertex class
+        probabilities.
+    pred_fn: Callable
+        The function that takes the IMCNN and returns the probabilities that the model uses to predict vertex classes.
+    class_dict: dict
+        A dictionary that maps class labels (integers) to their class names (strings).
+    signals_are_coordinates: bool
+        Whether the signals returned by 'old_dataset' are 3D-coordinates.
+    batch_size: int
+        The batch-size for DeepView.
+    max_samples: int
+        TODO
+    resolution: int
+        TODO
+    interpolations: int
+        TODO
+    lam: float
+        TODO
+    cmap: str
+        The colormap to use in DeepView.
+    metric: str
+        TODO
+    disc_dist: bool
+        Whether to use the discriminative distance in DeepView.
+    interactive: bool
+        TODO
+    title: str
+        The plot title
+    correction_file_name: str
+        The filename of the CSV-file where to store correction data.
+    """
     # --- Deep View Parameters ----
     amount_classes = len(class_dict)
     classes = np.arange(amount_classes)
