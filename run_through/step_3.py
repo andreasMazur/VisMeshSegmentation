c9c41bcc1474a5105b3cedc1765e26360b458ed3
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-07-30 15:06:11 +0200

    Adjusted gitignore and added run-through directory

---
+++ run_through/step_3.py
@@ -0,0 +1,26 @@
+from improve_mesh_segmentation.partnet_grasp.preprocess import preprocess_data
+from improve_mesh_segmentation.partnet_grasp.sampling.convert_partnet_labels import convert_partnet_labels
+from improve_mesh_segmentation.partnet_grasp.sampling.utils import PartNetDataset, ShapeNetDataset
+
+
+EXPERIMENT_DIRECTORY = "/home/andreas/Uni/projects/VisMeshSegmentation"
+DATASETS_PATH = f"{EXPERIMENT_DIRECTORY}/datasets"
+SAMPLED_PARTNET = f"{DATASETS_PATH}/sampled_partnet"
+PARTNET_GRASP = f"{DATASETS_PATH}/partnet_grasp"
+
+if __name__ == "__main__":
+    convert_partnet_labels(
+        partnet_dataset=PartNetDataset(f"{DATASETS_PATH}/PartNet-archive"),
+        shapenet_dataset=ShapeNetDataset(DATASETS_PATH),
+        aligned_archive_path=f"{DATASETS_PATH}/aligned_shapenet",
+        target_dataset_path=SAMPLED_PARTNET,
+        obj_class="Mug",
+        manual=False
+    )
+    preprocess_data(
+        data_path=SAMPLED_PARTNET,
+        target_dir=PARTNET_GRASP,  # will become a ZIP-file, i.e. 'partnet_grasp.zip'
+        processes=10,  # Adjust to how many CPU-cores you wish to use for preprocess
+        n_radial=5,
+        n_angular=8
+    )
