865305902125067986262816a6e09adb1154092f
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-09-16 11:14:46 +0200

    fixup! Added FAUST segmentation dataset

--- improve_mesh_segmentation/faust/segmentation_data/preprocess_faust.py
+++ improve_mesh_segmentation/faust/segmentation_data/preprocess_faust.py
@@ -2,7 +2,17 @@ from geoconv_examples.mpi_faust.data.preprocess_faust import preprocess_faust
 
 
 def get_faust_dataset(target_dir, registration_path, geodesic_diameters_path):
-    """Preprocess the FAUST dataset for segmentation tasks."""
+    """Preprocess the FAUST dataset for segmentation tasks.
+
+    Parameters
+    ----------
+    target_dir: str
+        The directory where the preprocessed dataset will be saved.
+    registration_path: str
+        The path to the directory containing the registered FAUST meshes.
+    geodesic_diameters_path: str
+        The path to the numpy file containing precomputed geodesic diameters.
+    """
     preprocess_faust(
         n_radial=3,
         n_angular=6,
