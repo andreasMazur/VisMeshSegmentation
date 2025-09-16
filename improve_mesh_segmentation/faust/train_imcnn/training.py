c75b135eddc52d655726de18e9334688cde95e9b
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-09-16 15:39:11 +0200

    Adjusted model size and set learning explicitly

--- improve_mesh_segmentation/faust/train_imcnn/training.py
+++ improve_mesh_segmentation/faust/train_imcnn/training.py
@@ -33,7 +33,7 @@ def training(dataset_path,
         kernel_size=(3, 6),
         segmentation_classes=8,
         template_radius=0.027744965069279016,
-        layer_conf=[(128, 6), (128, 6)]
+        layer_conf=[(32, 6), (32, 6)]
     ).to(device)
     train_data = FaustSegmentationDataset(
         path_to_zip=dataset_path,
@@ -80,7 +80,7 @@ def training(dataset_path,
         epoch_train_hist = model.train_loop(
             dataset=train_data,
             loss_fn=nn.CrossEntropyLoss(),
-            optimizer=torch.optim.Adam(model.parameters()),
+            optimizer=torch.optim.Adam(model.parameters(), lr=0.01),
             verbose=True,
             epoch=epoch
         )
