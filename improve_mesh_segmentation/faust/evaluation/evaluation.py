2b8ece0ff22dd2f176cba0c996bd6025203935f7
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-09-29 10:23:28 +0200

    Added functions to count corrections and fixed a bug that caused to add corrections

--- improve_mesh_segmentation/faust/evaluation/evaluation.py
+++ improve_mesh_segmentation/faust/evaluation/evaluation.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 
 import torch
 import numpy as np
+import pandas as pd
 import os
 
 
@@ -50,7 +51,10 @@ def compute_correction_statistics(dataset_path, segmentation_labels_path, loggin
     )
 
     # load corrections
-    corrections = np.load(corrections_path)
+    if corrections_path.endswith(".npy"):
+        corrections = np.load(corrections_path)
+    elif corrections_path.endswith(".csv"):
+        corrections = pd.read_csv(corrections_path, header=None).to_numpy()
 
     tps_counter = 0
     corrections_counter = 0
@@ -59,8 +63,10 @@ def compute_correction_statistics(dataset_path, segmentation_labels_path, loggin
         mesh_indices = torch.full((gt_o.shape[0],), fill_value=mesh_idx)
         vertex_indices = torch.arange(gt_o.shape[0])
 
+        # Create array that indicates which vertices are to be corrected according to method
         to_be_corrected = np.zeros((gt_o.shape[0]))
-        to_be_corrected[corrections[corrections[:, 0] == mesh_idx][:, 1]] = 1
+        mesh_corrections = corrections[corrections[:, 0] == mesh_idx]
+        to_be_corrected[mesh_corrections[:, 1]] = mesh_corrections[:, -1]
         to_be_corrected = torch.tensor(to_be_corrected)
 
         # 0: mesh index
@@ -79,5 +85,65 @@ def compute_correction_statistics(dataset_path, segmentation_labels_path, loggin
     noise_precision = tps_counter / corrections_counter
     noise_recall = tps_counter / to_be_found_counter
     noise_f1 = 2 / ((1 / noise_recall) + (1 / noise_precision))
-    print(f"Noise Precision: {noise_precision}, Noise Recall: {noise_recall}, Noise F1: {noise_f1}")
-    return noise_precision, noise_recall, noise_f1
+    print(
+        f"Corrected Vertices: {corrections_counter}, "
+        f"Noise Precision: {noise_precision}, "
+        f"Noise Recall: {noise_recall}, "
+        f"Noise F1: {noise_f1}"
+    )
+    return noise_precision, noise_recall, noise_f1, corrections_counter
+
+
+def count_mislabeled_points(dataset_path, segmentation_labels_path, logging_dir):
+    # Get noisy labels
+    noisy_dataset = FaustSegmentationDataset(
+        path_to_zip=dataset_path,
+        path_to_segmentation_labels=segmentation_labels_path,
+        logging_dir=logging_dir,
+        set_type=3,
+        only_signal=False,
+        device=None,
+        noise_level=None,
+        set_indices=None
+    )
+
+    # Create logging dir with clean segmentation labels
+    clean_logging_dir = f"{os.path.dirname(logging_dir)}/clean_faust_segmentation"
+    os.makedirs(clean_logging_dir, exist_ok=True)
+    clean_segmentation_labels = np.load(segmentation_labels_path)
+    np.save(f"{clean_logging_dir}/noisy_segmentation_labels.npy", np.tile(clean_segmentation_labels, (100, 1)))
+
+    # Get clean labels
+    clean_dataset = FaustSegmentationDataset(
+        path_to_zip=dataset_path,
+        path_to_segmentation_labels=segmentation_labels_path,
+        logging_dir=clean_logging_dir,  # No noise added
+        set_type=3,
+        only_signal=False,
+        device=None,
+        noise_level=None,
+        set_indices=None,
+        put_into_correct_order=True
+    )
+    total_mislabeled = 0
+    for mesh_idx, ((_, gt_o), (_, gt_c)) in tqdm(enumerate(zip(noisy_dataset, clean_dataset))):
+        # Count mislabeled points for mesh
+        mesh_mislabeled = torch.sum(gt_o != gt_c)
+
+        # Add to total count
+        total_mislabeled += mesh_mislabeled
+    return int(total_mislabeled)
+
+
+def count_correction_suggestions(corrections_path):
+    # load corrections
+    if corrections_path.endswith(".npy"):
+        corrections = np.load(corrections_path)
+    elif corrections_path.endswith(".csv"):
+        corrections = pd.read_csv(corrections_path, header=None).to_numpy()
+    else:
+        raise ValueError("The 'corrections_path' must end with .npy or .csv!")
+    if len(corrections.shape) > 2:
+        return int(corrections[:, -1].sum())
+    else:
+        return int(corrections.shape[0])
